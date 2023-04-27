fb36447ab8c904ced9193ec68180a2828bd9a07b
Jinny <74845448+igoodimin@users.noreply.github.com>
2023-04-27 14:35:25 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+jQuery.noConflict();
+jQuery( document ).ready(function( $ ) {
+	$(function(){
+		AOS.init();	
+	});
+});
+
+
+//tab
+function openBoard(evt, boardName) {
+	var i, x, tablinks;
+	x = document.getElementsByClassName("board");
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < x.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" opacity", "");
+	}
+	document.getElementById(boardName).style.display = "block";
+	evt.currentTarget.className += " opacity";
+}
